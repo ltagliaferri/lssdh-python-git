1b734e20dfc49662d747108a3d49f79358599daf
ltagliaferri <lisa.tagliaferri@gmail.com>
2021-05-12 13:51:22 -0400

    Remove redundant files

--- lssdh/freq_dist.py
+++
@@ -1,8 +0,0 @@
-import numpy
-import matplotlib.pyplot as plt
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-allwords = []
-officialstopwords = stopwords.words(english)
--- lssdh/text_wordcloud.py
+++
@@ -1,3 +0,0 @@
-import matplotlib.pyplot as plt
-from nltk.corpus import stopwords
-from wordcloud import WordCloud
